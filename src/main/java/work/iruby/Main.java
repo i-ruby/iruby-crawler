6fac8acb00a0637d3c6b79b0c4464f1106efb6ad
iRuby <1479867360@qq.com>
2020-12-22 22:56:57 +0800

    引入okhttp3 和 SpotBugs
    增加 okhttps 基本工具类及相关依赖
    增加 SpotBugs 插件检查

--- src/main/java/work/iruby/Main.java
+++ src/main/java/work/iruby/Main.java
@@ -1,9 +1,25 @@
 package work.iruby;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import work.iruby.utils.OkHttps;
+
+import java.io.IOException;
+
 /**
  * @author Ruby
  * @date 2020/12/22 12:20
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String sinaIndex = "https://sina.cn/";
+//        HashSet<String> isVisit = new HashSet<>();
+//        ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
+        String html = OkHttps.get(sinaIndex);
+        Document document =Jsoup.parse(html);
+        Elements elements = document.select("a");
+        elements.forEach(e -> System.out.println(e.attr("href")));
+
     }
 }
