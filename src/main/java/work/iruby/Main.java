af841e7d7380d45b7f453791206ee093763a714a
iRuby <1479867360@qq.com>
2020-12-22 13:55:55 +0800

    初始化项目
    增加 circleci 配置
    增加 checkstyle 配置
    更新 pom.xml
    增加基础测试

---
+++ src/main/java/work/iruby/Main.java
@@ -0,0 +1,9 @@
+package work.iruby;
+/**
+ * @author Ruby
+ * @date 2020/12/22 12:20
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/work/iruby/SmokeTest.java
@@ -0,0 +1,13 @@
+package work.iruby;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Ruby
+ * @date 2020/12/22 12:39
+ */
+public class SmokeTest {
+    @Test
+    public void test() {
+    }
+}
