f8863df0d240d026a9a409a09b081a31a800053e
iRuby <1479867360@qq.com>
2020-12-25 17:33:01 +0800

    构建一个普通的Elasticsearch 搜索引擎
    添加相关依赖,增加必须的字段和方法

---
+++ src/main/java/work/iruby/ElasticsearchMain.java
@@ -0,0 +1,42 @@
+package work.iruby;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author Ruby
+ * @date 2020/12/25 17:12
+ */
+public class ElasticsearchMain {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("please input a keyword");
+            Scanner scanner = new Scanner(System.in);
+            String keyword = scanner.nextLine();
+            search(keyword);
+        }
+    }
+
+    private static void search(String keyword) {
+        try (RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("http://121.4.73.4/", 9200, "http")))) {
+            SearchRequest request = new SearchRequest("news");
+            request.source(new SearchSourceBuilder().query(new MultiMatchQueryBuilder(keyword, "title", "content")));
+            SearchResponse searchResponse = client.search(request, RequestOptions.DEFAULT);
+            for (SearchHit hit : searchResponse.getHits()) {
+                System.out.println(hit.getSourceAsString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/work/iruby/entity/News.java
+++ src/main/java/work/iruby/entity/News.java
@@ -1,5 +1,7 @@
 package work.iruby.entity;
 
+import java.time.Instant;
+
 /**
  * @author Ruby
  * @date 2020/12/23 14:29
@@ -8,6 +10,8 @@ public class News {
     private String title;
     private String content;
     private String link;
+    private Instant createdAt;
+    private Instant updateAt;
 
     public String getTitle() {
         return title;
@@ -33,9 +37,40 @@ public class News {
         this.link = link;
     }
 
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Instant updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public News() {
+    }
+
     public News(String title, String content, String link) {
         this.title = title;
         this.content = content;
         this.link = link;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("News{");
+        sb.append("title='").append(title).append('\'');
+        sb.append(", content='").append(content).append('\'');
+        sb.append(", link='").append(link).append('\'');
+        sb.append(", createdAt=").append(createdAt);
+        sb.append(", updateAt=").append(updateAt);
+        sb.append('}');
+        return sb.toString();
     }
 }
